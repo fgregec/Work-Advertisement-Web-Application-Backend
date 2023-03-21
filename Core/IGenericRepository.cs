800cccd7c59028fcef69137e490a442d3d0c6514
Fran Gregec <fgregec@racunarstvo.hr>
2023-03-21 21:03:07 +0100

    Added Generic Repository

---
+++ Core/IGenericRepository.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core
+{
+    public interface IGenericRepository<T>
+    {
+        Task<T> GetByIdAsync(int id);
+        Task<IReadOnlyList<T>> ListAllAsync();
+        Task<T> GetEntityWithSpec();
+        Task<IReadOnlyList<T>> ListAsyncWithSpec();
+
+        void Add(T entity);
+        void Update(T entity);
+        void Delete(int id);
+    }
+}
