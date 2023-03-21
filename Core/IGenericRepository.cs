ff2d24ea5df1c125a968bddcd296555857b9312b
Fran Gregec <fgregec@racunarstvo.hr>
2023-03-21 21:17:46 +0100

    Fixed BaseEntity

--- Core/IGenericRepository.cs
+++ Core/IGenericRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Core.Entities;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,7 +7,7 @@ using System.Threading.Tasks;
 
 namespace Core
 {
-    public interface IGenericRepository<T>
+    public interface IGenericRepository<T> where T : BaseEntity
     {
         Task<T> GetByIdAsync(int id);
         Task<IReadOnlyList<T>> ListAllAsync();
