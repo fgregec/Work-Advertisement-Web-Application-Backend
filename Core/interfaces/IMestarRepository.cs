2397075383980f368266c80d83887d16158a5a9e
Nikola Biskup <nbiskup@racunarstvo.hr>
2023-03-31 23:29:08 +0200

    Added search methods for Mestar, not implemented completely

---
+++ Core/interfaces/IMestarRepository.cs
@@ -0,0 +1,15 @@
+﻿using Core.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Core.interfaces
+{
+    public interface IMestarRepository
+    {
+        Task<IEnumerable<Mestar>> GetMestarByName(string input_value);
+        Task<IEnumerable<Mestar>> GetMestarListByCategories(string[] categories);
+    }
+}
---
+++ Infrastructure/Repositories/MestarRepository.cs
@@ -0,0 +1,39 @@
+﻿using Core.Entities;
+using Core.interfaces;
+using Infrastructure.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace Infrastructure.Repositories
+{
+    public class MestarRepository : IMestarRepository
+    {
+        private readonly ApplicationContext _context;
+        public MestarRepository(ApplicationContext context)
+        {
+            _context = context;
+        }
+        public async Task<IEnumerable<Mestar>> GetMestarByName(string input_value)
+        {
+            if (string.IsNullOrEmpty(input_value))
+            {
+                throw new ArgumentNullException("input_value");
+            }
+
+            return await _context.Mestri.Where(x =>
+                   x.FirstName.ToLower().Contains(input_value.ToLower()) ||
+                   x.LastName.ToLower().Contains(input_value.ToLower())).ToListAsync();
+        }
+
+        public async Task<IEnumerable<Mestar>> GetMestarListByCategories(string[] categories) // add location as optional
+        {
+            if (categories == null || categories.Length == 0)
+            {
+                throw new ArgumentNullException(nameof(categories));
+            }
+
+            return await _context.Mestri.Where(mestar =>
+                   mestar.Categories.Any(category =>
+                   categories.Contains(category.Name))).ToListAsync();
+        }
+    }
+}
