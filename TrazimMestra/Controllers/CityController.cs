664d7348b72286d8c3b1acb91be1649ff6bd3a62
fgregec <frangregec2110@gmail.com>
2023-04-10 12:58:29 +0200

    Implemented city controller and CORS

---
+++ TrazimMestra/Controllers/CityController.cs
@@ -0,0 +1,24 @@
+﻿using Infrastructure.Data;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using TrazimMestra.Attributes;
+
+namespace TrazimMestra.Controllers
+{
+    public class CityController : BaseApiController
+    {
+        private readonly ApplicationContext _context;
+
+        public CityController(ApplicationContext context)
+        {
+            _context = context;
+        }
+
+        [HttpGet("all")]
+        public async Task<ActionResult<string>> GetAllCity()
+        {
+            var cities = await _context.Cities.ToListAsync();
+            return Ok(cities);
+        }
+    }
+}
--- TrazimMestra/Program.cs
+++ TrazimMestra/Program.cs
@@ -9,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
+{
+    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
+}));
+
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
@@ -21,6 +27,8 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+
+app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
