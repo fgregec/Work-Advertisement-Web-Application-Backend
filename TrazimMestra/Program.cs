e05f65f6891b1a3e7bd38505d24be006269dfb49
fgregec <frangregec2110@gmail.com>
2023-04-10 18:23:52 +0200

    Removed cors

--- TrazimMestra/Program.cs
+++ TrazimMestra/Program.cs
@@ -9,12 +9,6 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
-
-builder.Services.AddCors(p => p.AddPolicy("corspolicy", build =>
-{
-    build.WithOrigins("*").AllowAnyMethod().AllowAnyHeader();
-}));
-
 builder.Services.AddApplicationServices(builder.Configuration);
 
 var app = builder.Build();
@@ -27,8 +21,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-app.UseCors("corspolicy");
 
 app.UseAuthorization();
 
