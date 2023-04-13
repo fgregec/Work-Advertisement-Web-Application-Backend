7dcbdc6ed8cb7699284d59ceb64eb275e7214d16
Arjan Jakupi <ajakupi@racunarstvo.hr>
2023-04-13 15:24:47 +0200

    account fixes

--- TrazimMestra/Controllers/AccountController.cs
+++ TrazimMestra/Controllers/AccountController.cs
@@ -3,6 +3,7 @@ using Core.Interfaces;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.IdentityModel.Tokens;
 using System.Reflection.Metadata.Ecma335;
 using TrazimMestra.Dtos;
 
@@ -18,12 +19,12 @@ namespace TrazimMestra.Controllers
             _tokenService = tokenService;
         }
 
-        [HttpGet("getuser")]
+        [HttpGet]
         public async Task<ActionResult<User>> GetCurrentUser(Guid id)
         {
             var baseUser = await _repo.Users.FirstOrDefaultAsync(u => u.Id == id);
 
-            if(baseUser == null) 
+            if (baseUser == null)
             {
                 return NotFound();
             }
@@ -38,37 +39,57 @@ namespace TrazimMestra.Controllers
         }
 
         [HttpGet("login")]
-        public async Task<ActionResult<bool>> Login(string email, string password)
+        public async Task<ActionResult<string>> Login(string email, string password)
         {
             var baseUser = await _repo.Users.FirstOrDefaultAsync(u => u.Email == email);
 
-            if (baseUser == null) 
+            if (baseUser == null)
             {
-                return Ok(false);
+                return BadRequest("User does not exist");
             }
 
-            return Ok(baseUser.Password == password ? true:false);
+            return Ok(_tokenService.CreateToken(baseUser));
         }
 
-        [HttpGet("register")]
-        public async Task<ActionResult<string>> Register([FromQuery]RegisterDto registerUser) 
+        [HttpPost("register")]
+        public async Task<ActionResult<string>> Register([FromQuery] RegisterDto registerUser)
         {
-            if (ModelState.IsValid)
+
+            User user = new User
             {
-                User user = new User();
-                user.Id = Guid.NewGuid();
-                user.FirstName = registerUser.FirstName; 
-                user.LastName = registerUser.LastName;
-                user.Email = registerUser.Email;
-                user.Password = registerUser.Password;
-                user.CityID = registerUser.CityID;
+                Id = Guid.NewGuid(),
+                FirstName = registerUser.FirstName,
+                LastName = registerUser.LastName,
+                Email = registerUser.Email,
+                Password = registerUser.Password,
+                CityID = registerUser.CityID
+            };
 
-                await _repo.Users.AddAsync(user);
-                await _repo.SaveChangesAsync();
-                return Ok(_tokenService.CreateToken(user));
-            }
+            await _repo.Users.AddAsync(user);
+            await _repo.SaveChangesAsync();
+            return Ok(_tokenService.CreateToken(user));
 
-            return Ok(false);
+        }
+
+        [HttpPost("update")]
+        public async Task<ActionResult> Update([FromQuery] UpdateUserDto user)
+        {
+            User mappedUser = new User
+            {
+                Id = user.Id,
+                FirstName = user.FirstName,
+                LastName = user.LastName,
+                Email = user.Email,
+                CityID = user.CityID
+            };
+
+            if (!string.IsNullOrEmpty(user.Password))
+                mappedUser.Password = user.Password;
+
+            _repo.Users.Update(mappedUser);
+            await _repo.SaveChangesAsync();
+            return Ok();
+
         }
 
     }
---
+++ TrazimMestra/Dtos/UpdateUserDto.cs
@@ -0,0 +1,12 @@
+﻿namespace TrazimMestra.Dtos
+{
+    public class UpdateUserDto
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Password { get; set; }
+        public Guid CityID { get; set; }
+    }
+}
